a435066ca07446f77d917e0a5592633444fda0e9
Ldory <40350674+Ldory@users.noreply.github.com>
2018-07-20 17:50:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`رابط`,"http://twitch.tv/S-F")
+client.user.setGame(`I'm Bot Link .`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
